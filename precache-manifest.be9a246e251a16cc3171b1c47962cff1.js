b66bd1bef126dc14c6aece34014a4c1749a8d14e
Mario Johar <mjohar30@gmail.com>
2019-05-30 22:12:37 -0500

    Updates

---
+++ precache-manifest.be9a246e251a16cc3171b1c47962cff1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d6d7735938e6aa9e151c503d8b5530b",
+    "url": "/esports-client/index.html"
+  },
+  {
+    "revision": "2ba1ef81433380b5fe41",
+    "url": "/esports-client/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f9b71a5226eb5cfb6479",
+    "url": "/esports-client/static/css/main.05660fe8.chunk.css"
+  },
+  {
+    "revision": "2ba1ef81433380b5fe41",
+    "url": "/esports-client/static/js/2.7a0a19c9.chunk.js"
+  },
+  {
+    "revision": "f9b71a5226eb5cfb6479",
+    "url": "/esports-client/static/js/main.a7b7b158.chunk.js"
+  },
+  {
+    "revision": "4616ee232e0a9952bd30",
+    "url": "/esports-client/static/js/runtime~main.be614d54.js"
+  },
+  {
+    "revision": "e1e2fc81247017be0cd38484306a0f5d",
+    "url": "/esports-client/static/media/landing-esports.e1e2fc81.jpg"
+  },
+  {
+    "revision": "4b3639a08789f19e3b6d716246bf4876",
+    "url": "/esports-client/static/media/overwatch.4b3639a0.png"
+  }
+]);
